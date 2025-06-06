6369d6b21cf95e0d2099d1ac2b243c1c81daad69
Eric Stock <eric.stock@K202Domain.CSN.com>
2025-06-06 11:42:42 -0230

    First commit of movie project

---
+++ app/index.jsx
@@ -0,0 +1,30 @@
+import { Text, View, Image, Button } from "react-native";
+import { useState } from "react"
+import Movie from "../components/Movie.jsx";
+
+const t2 = require("../assets/images/terminator2.jpg");
+const titanic = require("../assets/images/titanic.jpg");
+const happy = require("../assets/images/happyGilmour.jpg")
+
+const images = { "t2" : t2, "titanic": titanic, "happy": happy};
+
+import movieData from "../assets/movies.json";
+console.log(movieData);
+
+export default function Index() {
+
+  const  [movieIndex, setMovieIndex] = useState(0);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Movie movieData={movieData[movieIndex]} image={images[movieData[movieIndex].image]} />
+      <Button title="Switch Movie" onPress={ () => setMovieIndex( (movieIndex + 1) % movieData.length)}/>
+    </View>
+  );
+}
